f50ac46527e51f1a0a9a2692a5847d504a7c82a1
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-01-07 15:45:53 +0000

    Working connection to API with correct data collection

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ async function newPlace(content="london") {
     const subject = content.toLowerCase();
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=f0d919f2079c4eabb02123135232212&q=${subject}`, {mode: 'cors'})
     const Data = await response.json();
+    getData(Data);
+    console.log(getData(Data));
     console.log(Data);
+    console.log(Data["current"]["condition"]["text"])
 }
 newPlace();
   
@@ -12,3 +15,25 @@ newPlace();
     console.log(content);
     newPlace(content);
   })
+
+function getData(Data) {
+  // Location
+  let country = Data["location"]["country"];
+  let localTime = Data["location"]["localtime"];
+  let name = Data["location"]["name"];
+  let region = Data["location"]["region"];
+  let timezone = Data["location"]["tz_id"];
+
+  // Current
+  let condition = Data["current"]["condition"]["text"];
+  let conditionImg = Data["current"]["condition"]["icon"];
+  let feelsLikeC = Data["current"]["feelslike_c"];
+  let gustMPH = Data["current"]["gust_mph"];
+  let precipMM = Data["current"]["precip_mm"];
+  let pressureMb = Data["current"]["pressure_mb"];
+  let tempC = Data["current"]["temp_c"];
+  let uv = Data["current"]["uv"];
+  let visibilityMiles = Data["current"]["vis_miles"];
+
+  return {country, localTime, name, region, timezone, condition, conditionImg, feelsLikeC, gustMPH, precipMM, pressureMb, tempC, uv, visibilityMiles}
+}
